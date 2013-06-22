8e76993878b28e344827aa42a7837b8cad4a2d4c
Jonathan Tavares <the.entomb@gmail.com>
2013-06-22 15:02:17 +0100

    licence and minify

--- kink.js
+++ kink.js
@@ -1,9 +1,34 @@
 /**
- * @module Ink.UI.Kink_1
+ * @module Ink.Plugin.Kink_1
  * @author entomb ( https://github.com/entomb/k-ink-interface ) and ported by inkdev AT sapo.pt
  * @version 1
+ *
+ * @license http://opensource.org/licenses/MIT 
+
+The MIT License (MIT)
+
+Copyright (c) 2013 Jonathan Tavares and other contributors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
  */
-Ink.createModule('Ink.Util.Kink',1,[
+Ink.createModule('Ink.Plugin.Kink',1,[
     'Ink.Dom.Browser_1',
     'Ink.Dom.Selector_1',
     'Ink.Dom.Event_1',
@@ -929,6 +954,8 @@ Ink.createModule('Ink.Util.Kink',1,[
             return new Result(Selector.select(param,context),param);
         }else if(param instanceof Array){
             return new Result(param);
+        }else if(param instanceof Function){
+            return new kink(param(context));
         }if(param instanceof Result){
             return param;
         }else{
