10dd2c02848f36c3bbab5c31e0a2d029eeeca4b1
Jonathan Tavares <the.entomb@gmail.com>
2013-09-25 05:36:03 +0100

    adding string helper and array helper functions

--- kink.js
+++ kink.js
@@ -95,9 +95,10 @@ Ink.createModule('Ink.Plugin.Kink',1,[
             rArray = param;
 
         }else if(typeof param === 'string'){ //query selector
-            rArray = InkSelector.select(param, context || document);
+            rArray = InkSelector.select(param, context);
         }else if(param instanceof Function){ //exec function
-            return kink(param(context));
+
+            return kink(param(context || kink));
 
         }else if(param instanceof kink.result){ //recursive much?
             return param;
@@ -186,6 +187,230 @@ Ink.createModule('Ink.Plugin.Kink',1,[
         }
         kink.extend(kink.result.prototype,extendObj,callback);
     };
+
+
+
+
+
+
+    /**
+     * @DISCLAIMER
+     * largely ispired in the jQuery core code. no need to re-invent the wheel here.
+     */
+
+
+
+
+    /**
+     * @module Helper
+     * @for kResult
+     */
+
+    /**
+     * @Class Helper
+
+
+
+
+     */
+    kink.extend({
+        /**
+         * @property kink.browser
+         * @for kink
+         * @public
+         */
+        browser: InkBrowser,
+
+
+        /**
+         * @method kink.type()
+         * @param  {mixed} element to inspect
+         * @return {string} returns the type of the object.
+         * @description
+
+    #type table:
+        true               === "boolean"
+        new Boolean()      === "boolean"
+        3                  === "number"
+        new Number(3)      === "number"
+        "test"             === "string"
+        new String("test") === "string"
+        function(){}       === "function"
+        []                 === "array"
+        new Array()        === "array"
+        new Date()         === "date"
+        new Error()        === "error"
+        /test/             === "regexp"
+
+
+         */
+        type: function(target){
+
+            if(target===null){
+                return 'null';
+            }
+
+            if(target===undefined){
+                return 'undefined';
+            }
+
+            var objectTypes = "object function array string number boolean date error regexp".split(" ");
+            var jsType = typeof target;
+
+            if(jsType==='object'){
+                /* global toString */
+                var evalType = (toString.call(target)).replace(/\[object|\s|\]/ig,'').toLowerCase();
+
+                return (this.inArray(evalType,objectTypes)>0) ?  evalType : jsType;
+
+            }else{
+                return jsType;
+            }
+        },
+
+
+        /**
+         * @method kink.now()
+         * @for kink
+         * @return {int} unix timestamp
+         */
+        now: function(){
+            return (new Date()).toTime();
+        },
+
+
+        /**
+         * @method kink.isJSON()
+         * @for kink
+         * @return {bool}
+         */
+        isJSON: InkString.isJSON,
+
+
+        /**
+         * @method kink.isFunction()
+         * @for kink
+         * @return {bool}
+         */
+        isFunction: function(target) {
+            return (this.type(target)==="function");
+        },
+
+
+        /**
+         * @method kink.isWindow()
+         * @for kink
+         * @return {bool}
+         */
+        isWindow: function(target) {
+            return (target===target.window);
+        },
+
+
+        /**
+         * @method kink.isNumeric()
+         * @for kink
+         * @return {bool}
+         */
+        isNumeric: function(target) {
+            return (target - parseFloat(target)>=0);
+        },
+
+
+        /**
+         * checks if an object is empty
+         * @param  {array|object} the element to inspect
+         * @return {bool}
+         */
+        isEmptyObject: function(target) {
+            var i;
+            for ( i in target ) {
+                return false;
+            }
+            return true;
+        },
+
+
+        /**
+         * Trows an error.
+         * @param  {string} msg Error menssage
+         * @return {void}
+         */
+        error: function(msg) {
+            throw new Error(msg);
+        },
+
+
+        /**
+         * Turns a dashed string into a camelCase one
+         * @param  {string} string some-dashed-string-like-this
+         * @return {string} stringLikeThis
+         */
+        camelCase: function( string ) {
+            return string.replace( /-([\da-z])/gi,function(all,letter) {
+                                                    return letter.toUpperCase();
+                                                });
+        },
+
+
+        /**
+         * Truncates a string, breaking words and adding ... at the end
+         *
+         * @method .truncateString()
+         * @param {String} string
+         * @param {Number} length - length limit for the string. String will be at most this big, ellipsis included.
+         * @return {String} string truncated
+         */
+        truncate: InkString.truncateString,
+
+
+        /**
+         * Remove spaces and new line from biggin and ends of string
+         *
+         * @method trim
+         * @param {String} string
+         * @return {String} string trimmed
+         */
+        trim: InkString.trim,
+
+
+        /**
+         * Remove spaces and new line from biggin and ends of string
+         *
+         * @method trim
+         * @param {String} string
+         * @return {String} string trimmed
+         */
+        stripTags: InkString.stripTags,
+
+
+        /**
+         * Convert listed characters to HTML entities
+         *
+         * @method htmlEntitiesEncode
+         * @param {String} string
+         * @return {String} string encoded
+         */
+        htmlEntitiesencode: InkString.htmlEntitiesEncode,
+
+
+        /**
+         * Convert listed HTML entities to character
+         *
+         * @method htmlEntitiesDecode
+         * @param {String} string
+         * @return {String} string decoded
+         */
+        htmlEntitiesDecode: InkString.htmlEntitiesDecode,
+
+
+        /**
+         * Just an empty function
+         * @return {void}
+         */
+        noop: function() {}
+
+     });
 
 
 
@@ -374,8 +599,6 @@ Ink.createModule('Ink.Plugin.Kink',1,[
 
 
 
-
-
     kink.extend({
 
         /**
@@ -397,7 +620,132 @@ Ink.createModule('Ink.Plugin.Kink',1,[
             return InkArray.some( InkArray.convert(arr) ,function(element,index){
                 return callback.call(element,element,index,arr);
             });
-        }
+        },
+
+        /**
+         * @method kink.filter()
+         * @for kink
+         */
+        filter: function(arr,callback) {
+            return arr.filter(function(element,index){
+                callback.call(element,element,index);
+            });
+        },
+
+        /**
+         * @method kink.filter()
+         * @for kink
+         */
+        map: function(arr,callback) {
+            var mapped = [];
+            this.each(arr,function(element,index){
+                var value = callback.call(element,element,index);
+                if(value!==undefined && value!==null){
+                    mapped.push(value);
+                }
+            });
+            return mapped;
+        },
+
+        /**
+         * @method kink.filter()
+         * @for kink
+         */
+        grep: function(arr,callback) {
+            return this.map(arr,function(element,index){
+                var value = callback.call(element,element,index);
+                if(value){
+                    return element;
+                }else{
+                    return null;
+                }
+            });
+        },
+
+        /**
+         * @method kink.makeArray()
+         * @for kink
+         */
+        makeArray: function(target){
+            return InkArray.convert(target);
+        },
+
+        /**
+         * @method kink.inArray()
+         * @for kink
+         */
+        inArray: function(element, arr) {
+            var i = this.keyValue(element,arr,true);
+            return (i!==false) ? i : -1; // -1 is here to replicate Array.indexOf
+        },
+
+        keyValue: function(element,arr,stopAtFirst){
+            return InkArray.keyValue(element,arr,stopAtFirst);
+        },
+
+        /**
+         * @method kink.isArray()
+         * @for kink
+         */
+        isArray: function(target){
+            return (this.type(target)==="array");
+        },
+
+        /**
+         * Merge two arrays and return the result
+         *
+         * @method kink.merge()
+         * @for  kink
+         * @param  {array} arr1 target
+         * @param  {array} arr2 source
+         * @return {array} arr1+arr2
+         */
+        merge: function(arr1,arr2){
+            arr1 = this.makeArray(arr1);
+            arr2 = this.makeArray(arr2);
+
+            for(var i in arr2){
+                arr1.push(arr2[i]);
+            }
+
+            return arr1;
+        },
+
+
+        /**
+         * Intersect two arrays and return the result
+         *
+         * @method kink.intersect()
+         * @for  kink
+         * @param  {array} arr1 target
+         * @param  {array} arr2 source
+         * @return {array} arr1+arr2
+         */
+        intersect: function(arr1,arr2){
+            arr1 = this.makeArray(arr1);
+            arr2 = this.makeArray(arr2);
+
+            return InkArray.intersect(arr1,arr2);
+        },
+
+        /**
+         * removes duplicares in an array
+         *
+         * @method kink.unique()
+         * @for  kink
+         * @param  {array} arr source
+         * @return {array} unique elements from arr
+         */
+        unique: function(arr){
+
+            arr = this.makeArray(arr);
+
+            return this.grep(arr,function(element,index){
+                return (kink.inArray(element,arr)===index);
+            });
+
+        },
+
 
     });
 
@@ -425,7 +773,29 @@ Ink.createModule('Ink.Plugin.Kink',1,[
          */
         some: function(callback){
             return kink.some(this,callback);
-        }
+        },
+
+        /**
+         * @method .makeArray()
+         * @for kResult
+         */
+        makeArray: function(){
+            return this.get();
+        },
+
+        /**
+         * @method .filter()
+
+        some: function(option){
+
+            if(option instanceof Function){
+
+            }else{
+                var filterElements = kink(option);
+                this.filter(function(){
+                });
+            }
+        }*/
 
     });
 
