14c6a6571df5abbcb6dae0ce9215677c1c5d67c7
Jonathan Tavares <the.entomb@gmail.com>
2013-09-27 04:09:38 +0100

    adding dom.js to prepare for the next batch of methods

--- kink.js
+++ kink.js
@@ -1007,6 +1007,41 @@ Ink.createModule('Ink.Plugin.Kink',1,[
 
 
     /**
+     * @module DOM
+     * @for kResult
+     */
+
+    /**
+     * @DOM Element
+     *
+     */
+    kink.result.extend({
+
+        /**
+         * @method .remove()
+         * @param  {string} selector elements to filter before the remove
+         * @returns {kResult}
+         * @chainable
+         */
+        remove: function(selector){
+            if(kink.type(selector,'string')){
+                return this.find(selector).each(function(i,elem){
+                    elem.remove();
+                });
+            }else{
+                return this.each(function(i,elem){
+                    elem.remove();
+                });
+            }
+        }
+
+
+    });
+
+
+
+
+    /**
      * @module CSS
      * @for kResult
      *
